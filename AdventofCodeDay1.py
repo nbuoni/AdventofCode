1144146dd6e3c2aa6eae88f0648c9cdc47f785b5
mchonofsky <mchonofsky@gmail.com>
2021-12-04 20:42:36 -0700

    further tidy of var names and usage

--- AdventofCodeDay1.py
+++ AdventofCodeDay1.py
@@ -1,8 +1,8 @@
 # Given a fabricated list of ocean depths detected by a sonar sweep, this counts the number of times a depth measurement increases from the previous measurement.
 from constants import depths
 
-y = 0
-for x in range(1, len(depths)):
-    if depths[x] > depths[x-1]:
-        y += 1
-print(y)
+number_of_increases = 0
+for x in range(len(depths) - 1):
+    if depths[x + 1] > depths[x]:
+        number_of_increases += 1
+print(number_of_increases)
--- AdventofCodeDay1Part2.py
+++ AdventofCodeDay1Part2.py
@@ -2,15 +2,16 @@
 from constants import depths
 
 # creates a new list containing the sums of a three-measurement sliding window
-third_depths = list()
+
+depths_three_sum = []
 for x in range(2, len(depths)):
-		third_depths.append(depths[x] + depths[x-1] + depths[x-2])
+		depths_three_sum.append(depths[x] + depths[x-1] + depths[x-2])
 
 # counts the number of times the sum of measurements in this sliding window increases
-y = 0
-for x in range(1, len(third_depths)):
-    if third_depths[x] > third_depths[x-1]:
-        y += 1
-print(y)
+number_of_increases = 0
+for x in range(len(depths_three_sum) - 1):
+    if depths_three_sum[x] > depths_three_sum[x-1]:
+        number_of_increases += 1
+print(number_of_increases)
 
 
