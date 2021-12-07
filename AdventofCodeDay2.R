c09dec0db505692453c312e37be400229bbf1db5
Nina Buoni <buoni.nina@gmail.com>
2021-12-07 17:37:37 -0500

    adds comments to original solution

--- AdventofCodeDay2.R
+++ AdventofCodeDay2.R
@@ -1,17 +1,24 @@
-# load packages
+# loads packages
 library(googlesheets4)
 library(dplyr)
-# read submarine movement data into list 'submarine_movement' from google sheets
+
+# reads submarine movement data into list 'submarine_movement' from google sheets
 submarine_movement <- range_read("https://docs.google.com/spreadsheets/d/1YUBHAw4HpHMqiNiF4CG9QZzwXygiOZWdph2Jjda_Kj8/edit#gid=0", 
                                  col_types = NULL)
 
-# create dataframe of 'submarine_movement' with columns 'direction' and 'magnitude' columns
+# creates dataframe of 'submarine_movement' with columns 'direction' and 'magnitude' columns
 submarine_movement <- as.data.frame(do.call(cbind, submarine_movement))
 
+# changes data type of 'magnitude' column from character to int
 submarine_movement$magnitude <- as.integer(submarine_movement$magnitude)
 
+# sets horizontal and vertical position equal to 0
 horizontal_position = 0
 vertical_position = 0
+
+#loops through submarine_movement to determine final horizontal and vertical position given that
+#'forward X' increases horizontal position by X units, 'down X' increases the depth by X units, and
+#'up X' decreases the depth by X units
 
 for (cell in 1:1000) {
   if (submarine_movement$direction[cell] == "forward") {
@@ -26,6 +33,7 @@ for (cell in 1:1000) {
     }
 }
 
-final_answer = horizontal_position*vertical_position
+# calculates final position by multiplying final horizontal position by final depth
+final_position = horizontal_position*vertical_position
 
 
