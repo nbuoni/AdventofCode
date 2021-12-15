65d94b72d56a821aa9bbf70a77b8d9ea77b759c8
NinaBuoni <buoni.nina@gmail.com>
2021-12-14 21:13:37 -0500

    simplify code by adding functions

--- AdventofCodeDay3.py
+++ AdventofCodeDay3.py
@@ -7,37 +7,29 @@ submarine_bits = pd.read_csv(r'/Users/ninabuoni/Documents/AdventofCode/AdventofC
                              header = None,
                              index_col = False)
 
-# define gamma rate and epsilon rate as empty strings
-gamma_rate = ""
-epsilon_rate = ""
+# define function to obtain most common column value of data frame containing bits given data frame and column index
+def get_most_common_value(df, column_index):
+    ratio = submarine_bits[column_index].sum()/len(submarine_bits)
+    most_common_value = round(ratio)
+    return most_common_value
 
-#
+# define function to obtain least common column value of data frame containing bits given data frame and column index
+def get_least_common_value(df, column_index):
+    ratio = submarine_bits[column_index].sum()/len(submarine_bits)
+    least_common_value = round(1- ratio)
+    return least_common_value
+
 """  determine each bit in the gamma rate by finding most common bit in
 corresponding position of all numbers in the diagnostic report and determine each
 bit in the epsilon rate by finding least common bit in corresponding position of
 all numbers in the diagnostic report"""
 
-for i in range(len(submarine_bits.columns)):
-    zero_count = 0
-    one_count = 0
-
-    for j in range(len(submarine_bits)):
-        if submarine_bits.iloc[j,i] == 0:
-            zero_count += 1
-        else:
-            one_count += 1
-
-    if zero_count > one_count:
-        gamma_rate = gamma_rate + '0'
-        epsilon_rate = epsilon_rate + '1'
-
-    else:
-        gamma_rate = gamma_rate + '1'
-        epsilon_rate = epsilon_rate + '0'
+gamma_rate = "".join([str(get_most_common_value(submarine_bits, i)) for i in range(len(submarine_bits.columns))])
+epsilon_rate = "".join([str(get_least_common_value(submarine_bits, i)) for i in range(len(submarine_bits.columns))])
 
 # print gamma rate and epsilon rate
 print(f"gamma rate = {gamma_rate}")
-print(f"epsilon rate= {epsilon_rate}")
+print(f"epsilon rate = {epsilon_rate}")
 
 # convert gamma rate and epsilon rate to decimal from binary
 gamma_rate_decimal = int(gamma_rate, 2)
@@ -46,5 +38,4 @@ epsilon_rate_decimal = int(epsilon_rate, 2)
 # calculate power consumption by multiplying gamma rate by epsilon rate
 power_consumption = gamma_rate_decimal*epsilon_rate_decimal
 
-# print power consumption
 print(f"power_consumption= {power_consumption}")
